97f3af26791fc72b4b4fc8139203d212fbfac0e4
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2019-02-11 11:41:29 +0100

    Add files via upload

---
+++ gameRef-python/Game.py
@@ -0,0 +1,15 @@
+import random
+import math
+import copy
+import numpy as np
+
+## ----------------
+
+from Parameters import get_args
+from Rules import AttackValide
+from draftPhase import Draft
+from Player import Player
+from Card import Card
+
+## ----------------
+
